09ff10d1cb08f48bfe7143f73cd0646ba1a58087
Rolarg29 <rolurmzguerra@gmail.com>
2022-05-01 18:27:40 -0500

    Incluir modulo propio

--- nodepractice/myfirst.js
+++ nodepractice/myfirst.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = required('./myfirstmodule');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! este es un archivo de prueba');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
 }).listen(8080);
